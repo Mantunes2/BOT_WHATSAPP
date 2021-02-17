6789effa4b03761ad6e2ababd52156e7e9ac9d26
Matheus Antunes <38018748+Mantunes2@users.noreply.github.com>
2021-02-17 00:09:14 -0300

    BOT_WHATSAPP
    Bot programado para enviar mensagem automaticamente.
    
    requisitos:
    
    pip install pywhatkit

--- Botwhatsapp.py
+++ Botwhatsapp.py
@@ -4,12 +4,12 @@ import keyboard
 import time
 from datetime import datetime
 # 2. Definir para qual contato enviaremos as msgs
-Contatos = ['+5521979354287']
+Contatos = ['Número para enviar a mensagem']
 # 3. Definir intervalo de envio
 while len(Contatos) >= 1:
     # Enviar mensagem.
-    pywhatkit.sendwhatmsg(Contatos[0],"Drugs(MP3_128K)_1",datetime.now().hour,datetime.now().minute + 2)
+    pywhatkit.sendwhatmsg(Contatos[0],"Mensagem a ser enviada",datetime.now().hour,datetime.now().minute + 2)
     del Contatos[0]
     time.sleep(60)
-    keyboard.press_and_release('Ctrl + h')
+    keyboard.press_and_release('Ctrl + w')
 # 4. Testar!
